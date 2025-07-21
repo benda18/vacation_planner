fa9f08fdc41912bdc03e9c311987abbff88f7180
Tim Bender <bendertj@gmail.com>
2025-07-20 21:23:53 -0400

    peroidic

--- R/generate_location_df.R
+++ R/generate_location_df.R
@@ -106,28 +106,105 @@ gen_dest <- function(name, addr = NA, desc,
   return(out)
 }
 
-cgpt_prompt <- "plan a road trip for me leaving durham, nc, returning to durham, nc and visiting the following locations:
+# chatgpt  prompts----
 
-1. georgia aquarium in atlanta
+gemini_all <- "plan a road trip. i'll be driving a 2019 chevrolet bolt ev with DC fast charging. 
 
-2. majestic caverns in alabama
+Depart Durham NC at 9am on July 21st, 2025.  
+Spend 2 hours at carowinds 
+arrive in atlanta, GA near the georgia aquarium.
+stay at a hotel overnight. 
 
-3. the unclaimed baggage store in alabama
 
-4. huntsville alabama rocket center
+On Tuesday, depart the hotel around 9am. 
+Visit the georgia aquarium. 
+visit Six Flags Over Georgia for 2-3 hours. 
+Drive to Toomer's Corner in Auburn, Al.  Have a meal in Auburn. 
+Drive to Montgomery, Al.  Stay in a hotel in Montgomery. 
 
-5. birmingham, al
+On Wednesday, visit the civil rights meorial in montgomery. 
+visit the national memorial for peace and justice.
+drive to Selma and spend 30-60 minutes exploring the city. 
+stop at orr park to view the tinglewood carvings. 
+drive to Birmingham, Al. stay in a hotel in birmingham. 
 
-6. montgomery, al
+On Thursday spend time exploring Birmingham Al. 
+Visit the Museum of Fond Memories at Reed Books.
+Visit the Sloss Furnaces visitor museum and site. 
+Drive to Huntsville, Al and visit the US Rocket and Space Center.
+Stay in hotel in Huntsville. 
 
-7. selma, al
+On Friday, drive from Huntsville to Asheville, NC.  
+along the way, stop at the unclaimed luggage store at 509 W Willow St, Scottsboro, AL 35768 for an hour. 
+Along the way, drive the tail of the dragon and stop at deal's gap motorcycle resort for 30 minutes.
+Stay in a hotel in Asheville.
 
-vehicle type is 2019 chevrolet bolt ev. optimize the route for the shortest travel time
+On Saturday, drive from Asheville to Durham, NC."
+
+
+# gpt_monday <- "plan a road trip.  
+# i'll be driving a 2019 chevrolet bolt ev with a 60kWh battery pack that charges max speed at 50kW and about 30kW on average from 20% SoC to 80% Soc.  
+# Depart 1210 laurel meadows drive, durham nc tomorrow morning at 9am. 
+# 
+# spend 2 hours at carowinds
+# arrive in atlanta, GA near the georgia aquarium"
+# 
+# gpt_tuesday <- "plan a road trip.  
+# i'll be driving a 2019 chevrolet bolt ev.  
+# depart georgia aquarium around 9am. 
+# spend 2-3 hours at Six Flags Over georgia. 
+# drive to toomer's corner in Auburn, Al.
+# drive to Montgomery Alabama"
+# 
+# gpt_wednesday <- "plan a road trip.  
+# i'll be driving a 2019 chevrolet bolt ev.
+# 
+# depart Montgomery, Al around 2pm. 
+# drive to Selma, Al for 30-60 minutes. 
+# drive to Birmingham, Al"
+# 
+# gpt_thursday <- "plan a road trip.  
+# i'll be driving a 2019 chevrolet bolt ev.
+# 
+# depart Birmingham, Al around 2pm. 
+# drive to Huntsville, Al"
+# 
+# gpt_friday <- "plan a road trip.  
+# i'll be driving a 2019 chevrolet bolt ev.
+# 
+# depart Huntsville, Al around 2pm. 
+# drive to Deals Gap motorcycle resort. 
+# drive to Asheville, NC"
+# 
+# gpt_saturday <- "plan a road trip.  
+# i'll be driving a 2019 chevrolet bolt ev.
+# 
+# depart Asheville, NC around 9am. 
+# drive to Durham, NC"
+# 
+# cgpt_prompt <- "plan a road trip for me leaving durham, nc, returning to durham, nc and visiting the following locations:
+# 
+# 1. georgia aquarium in atlanta
+# 
+# 2. majestic caverns in alabama
+# 
+# 3. the unclaimed baggage store in alabama
+# 
+# 4. huntsville alabama rocket center
+# 
+# 5. birmingham, al
+# 
+# 6. montgomery, al
+# 
+# 7. selma, al
+# 
+# vehicle type is 2019 chevrolet bolt ev. optimize the route for the shortest travel time
 
 
 recommend good record stores, bookstores, and skateparks along the way "
 
 
+# Locations----
 map_skateparks <- c("")
 
 map_stops <- c("509 W Willow St, Scottsboro, AL 35768", #Unclaimed Baggage Center, Scottsdale AL", 
@@ -147,16 +224,26 @@ map_dests <- rbind(gen_dest("NASA US Space and Rocket Center",
                             desc = "visit", 
                             lat = 32.40536, 
                             lon = -87.018485), 
+                   gen_dest(name = "Six Flags Over Georgia", 
+                            addr = "275 Riverside Pkwy, Austell, GA 30168", 
+                            desc = "visit", 
+                            lon = -84.5500, 
+                            lat = 33.7683),
                    gen_dest(name = "Home", 
                             addr = "1210 Laurel Meadows Drive, Durham, NC", 
                             desc = "visit", 
                             lon = -78.91775, 
                             lat = 36.04909), 
                    gen_dest(name = "Georgia Aquarium", 
-                            addr = "225 Baker St NW, Atlanta, GA 30313", 
+                            addr = "225 Baker St, Atlanta, GA 30313", 
                             desc = "visit", 
                             lon = NA, 
-                            lat = NA))
+                            lat = NA), 
+                   gen_dest(name = "Carowinds", 
+                            addr = NA, 
+                            desc = "visit", 
+                            lon = -80.94167, 
+                            lat = 35.10278))
 
 map_cities <- rbind(gen_dest("Montgomery, AL", 
                        addr = NA, 
@@ -185,9 +272,10 @@ map_cities <- rbind(gen_dest("Montgomery, AL",
                              lat = 35.21265)
                     )
 
-censusxy::cxy_oneline("100 N college st, auburn, al")[c("coordinates.x", 
+censusxy::cxy_oneline("275 Riverside, Austell, GA")[c("coordinates.x", 
                                                       "coordinates.y")]
 
+# Data Tidy----
 for(i in 1:nrow(map_dests)){
   if(is.na(map_dests$x[i] & !is.na(map_dests$loc_addr[i]))){
     out1 <- (censusxy::cxy_oneline(map_dests$loc_addr[i]))
@@ -198,14 +286,15 @@ for(i in 1:nrow(map_dests)){
   }
 }
 
-#censusxy::cxy_oneline(map_dests$loc_addr[1])
-#cxy_oneline("1 Tranquility Base, Huntsville, AL 35805")
+# High-level days
+
+# Mapping----
 
 leaflet() %>%
   addTiles() %>%
   addMarkers(lng = map_dests$x,
              lat = map_dests$y,
-             label = map_dests$loc_name) %>%
+             label = map_dests$loc_name) #%>%
   addCircleMarkers(lng = map_cities$x, 
              lat = map_cities$y, 
              label = map_cities$loc_name, 
